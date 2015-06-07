e0375961ab0bd0543edc10c248f894c7378aa8b5
Rafael Cano <miriadax.rcanop@rafacano.es>
2015-06-07 23:31:24 +0200

    Módulo 6 P2P obligatorio
    Pagina Author  y link a git con el proyecto.

--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,9 @@ router.get('/', function(req, res) {
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
+// añadimos ruta acerca de
+router.get('/author', function (req, res) {
+  res.render('author');
+});
+
 module.exports = router;
