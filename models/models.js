2052fe1d5a2028522cc6d2feac97fd15ce5c8d9f
Rafael Cano <miriadax.rcanop@rafacano.es>
2015-06-12 19:07:31 +0200

    Despliegue DB en Heroku
    Asociar Postgres para producción, y SQLite para desarrollo.

--- models/models.js
+++ models/models.js
@@ -1,12 +1,36 @@
 ﻿var path = require('path');
 
+// Configurar la base datos.
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+
+var DB_name = (url[6] || null);
+var DB_user     = (url[2] || null);
+var DB_pwd      = (url[3] || null);
+var DB_protocol = (url[1] || null);
+var DB_dialect  = (url[1] || null);
+var DB_port     = (url[5] || null);
+var DB_host     = (url[4] || null);
+
+var storage = process.env.DATABASE_STORAGE;
+
 // Cargar modelo ORM
 var Sequelize = require('sequelize');
 
 // Usar BD SQLite
-var db = new Sequelize(null, null, null, 
-  { dialect: 'sqlite', storage: 'quiz.sqlite' }
-);
+//var db = new Sequelize(null, null, null, 
+//  { dialect: 'sqlite', storage: 'quiz.sqlite' }
+//);
+var db = new Sequelize(DB_name, DB_user, DB_pwd, 
+ {
+  dialect: DB_dialect,
+  protocol: DB_protocol,
+  port: DB_port,
+  host: DB_host,
+  storage: storage,   // sólo SQLite (.env)
+  omitNull: true      // sólo Postgres
+});
+
+
 // importamos la definición de la tabla Quiz en quiz.js
 var Quiz = db.import(path.join(__dirname, 'quiz'));
 
