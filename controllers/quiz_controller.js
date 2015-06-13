44b89df75908c1227d3303b87b3e5a94115ca064
Rafael Cano <miriadax.rcanop@rafacano.es>
2015-06-13 11:13:14 +0200

    Añadir autoload a las direcciones con parámetro **quizId**
    Se añada la función load para que carge antes de llamar al enrutador
    correspondiente, y en ella se hace comprobación de que exista el id de
    pregunta. Generando error en caso de que no exista.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,35 +1,37 @@
 var models = require('../models/models.js');
 
+// Autoload - factoriza el código si la ruta contiene :quizId
+exports.load = function(req, res, next, quizId) {
+  models.Quiz.findById(quizId).then(
+    function (quiz) {
+      if (quiz) {
+        req.quiz = quiz;
+        next();
+      } else {
+        next(new Error("No existe quizId = " + quizId));
+      }
+    }).catch(function(error) { next(error); });
+}; 
 // GET /quizes
 exports.index = function(req, res) {
   models.Quiz.findAll().then(function (quizes) {
     res.render('quizes/index.ejs', { quizes: quizes });
-  });
+  }).catch(function (error) { next(error); });
 };
 // GET /quizes/show
 exports.show = function (req, res) {
   models.Quiz.findById(req.params.quizId).then(function (quiz) {
-    console.log(req.params.quizId);
-    console.log(quiz);
-    res.render('quizes/show', { quiz: quiz });
-  });
+    res.render('quizes/show', { quiz: req.quiz });
+  }).catch(function (error) { next(error); });
 };
 
 // GET /quizes/answer
 exports.answer = function (req, res) {
   models.Quiz.findById(req.params.quizId).then(function (quiz) {
-    if (req.query.respuesta === quiz.respuesta) {
-      res.render('quizes/answer', {
-        quiz: quiz, 
-        respuesta: true
-      });
-    }
-    else {
-      res.render('quizes/answer', {
-        quiz: quiz, 
-        respuesta: false
-      });
-    }
-  });
-  
+    var obj = {
+      quiz: quiz,
+      respuesta: (req.query.respuesta === req.quiz.respuesta)
+    };
+    res.render('quizes/answer', obj);
+  }).catch(function (error) { next(error); });
 };
